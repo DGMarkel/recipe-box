699edbf031b3fc023e0ffcf6a3f3337d829132f5
github username <github email address>
2019-03-12 18:10:31 -0400

    add methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ApplicationRecord
   validates_uniqueness_of :username
   has_secure_password
+  has_secure_token :auth_token
+
+  def invalidate_token
+    self.update_columns(auth_token: nil)
+  end
+
+  self.validate_login(username, password)
+    user = User.find_by(username: username)
+    if user && user.validate(password)
+      user
+    end
+  end
 
 end
