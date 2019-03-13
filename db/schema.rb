ca28ca81f00f886b1ecf948507ec752c554139ca
github username <github email address>
2019-03-12 21:16:42 -0400

    create monsters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_180001) do
+ActiveRecord::Schema.define(version: 2019_03_13_011356) do
+
+  create_table "monsters", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_monsters_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
