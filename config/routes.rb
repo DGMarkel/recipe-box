c59478416a81f5589618e62b4ae400709497ea5e
github username <github email address>
2019-03-12 21:22:46 -0400

    seed monsters, add monsters route, build monsters controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   get '/profile' => 'users#profile'
-
+  resources :monsters
   resources :users
 
 end
