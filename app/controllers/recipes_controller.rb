6d844cfbbf20854026ef29df4bd553c8965ebae3
github username <github email address>
2019-03-18 16:52:49 -0400

    build out recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,35 @@
-class RecipesController < ApplicationController
+class RecipesController < ApiController
+  before_action :require_login, except: [:index, :show]
+
+  def index
+    recipes = Recipes.all
+    render json: { recipes: recipes }
+  end
+
+  def show
+    recipe = Recipe.find(params[:id])
+    render json: { recipe: recipe }
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    recipe.user = current_user
+    if recipe.save
+      render json: {
+        message: 'ok',
+        recipe: recipe
+      }
+    else
+      render json: {
+        message: 'Could not create recipe'
+      }
+    end
+  end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:title, :description)
+    end
+
 end
