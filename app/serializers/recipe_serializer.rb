2333033b519a2cbb2dd7cbe8fabb67c7540368f3
github username <github email address>
2019-04-24 15:14:18 -0400

    recipe_totals attribute not working, remove for now

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,4 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :image_url, :recipe_totals, :creator_name
+  attributes :id, :title, :description, :image_url, :creator_name
   has_many :ingredients
 end
