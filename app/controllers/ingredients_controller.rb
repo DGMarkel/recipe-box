aa3336218bba52345a6d650519dcab705ec7de46
github username <github email address>
2019-05-01 01:23:59 -0400

    delete unused action

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -8,12 +8,6 @@ class IngredientsController < ApiController
     ingredient.update(ingredient_params[:ingredient_data])
   end
 
-  def search
-    recipes = []
-    Ingredient.where("food_name like ?", "%#{ingredient_params[:search_term]}%").each { |ingredient| recipes.push(ingredient.recipe) }
-    render json: recipes
-  end
-
   def destroy
     ingredient = Ingredient.find_by(food_name: ingredient_params[:ingredient_data][:food_name], recipe_id: ingredient_params[:recipeID])
     ingredient.destroy
