348ff4a763ab29c2ffa26e7750456cdb48fae8e1
github username <github email address>
2019-05-01 17:32:02 -0400

    save because i got an error telling me to do so

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,4 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :image_url, :recipe_totals, :nutritional_data_per_serving, :creator_name
+  attributes :id, :title, :description, :image_url, :servings, :recipe_totals, :nutritional_data_per_serving, :creator_name
   has_many :ingredients
 end
