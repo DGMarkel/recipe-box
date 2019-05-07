fe1b8c679eb6f744bed12601acc0ba559e1636b8
github username <github email address>
2019-05-07 19:46:58 -0400

    add servings to serializer, remove recipe_totals

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,4 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :image_url, :recipe_totals, :creator_name
+  attributes :id, :title, :description, :image_url, :servings, :creator_name
   has_many :ingredients
 end
